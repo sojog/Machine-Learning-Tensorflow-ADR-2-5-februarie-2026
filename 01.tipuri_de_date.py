41dfc3214feca01e0611b902d0d1482eb3445d7e
Silviu Ojog <sil.ojog@gmail.com>
2026-02-02 11:32:28 +0200

    Python intro

---
+++ 01.tipuri_de_date.py
@@ -0,0 +1,17 @@
+
+## Python este interpretat, loosly typed
+
+x = True
+print(x, type(x))
+
+x = 1
+print(x, type(x))
+
+x = 2.3
+print(x, type(x))
+
+x = "salut"
+print(x, type(x))
+
+x = None
+print(x, type(x))
